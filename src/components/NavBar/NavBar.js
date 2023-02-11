df8832748cf6a90ce6e9ec26a42f9fde0c80ba0e
Divya Ambadipudi <divya.ambadipudi@clever.com>
2023-02-11 14:07:33 -0800

    Added nav bar to login page as well

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,26 +6,26 @@ import "./NavBar.css";
 const NavBar = ({ isValidLogin, authedUserName, dispatch }) => {
     const onSubmitLogin = (e) => {
         e.preventDefault();
-        dispatch(logoutUser());
+        if (isValidLogin && authedUserName) {
+            dispatch(logoutUser());
+        }
     }
 
     return (
         <form onSubmit={onSubmitLogin}>
             <div className="NavBar-container">
                 <div className="NavBar-header"><h1>Employee Polls</h1></div>
-                {isValidLogin && authedUserName ? (
-                    <div className="NavBar-navigation">
-                        <div>
-                            <span className="NavBar-element"><Link to="/">Home</Link></span>
-                            <span className="NavBar-element"><Link to="/leaderboard">Leaderboard</Link></span>
-                            <span className="NavBar-element"><Link to="/add">Add Question</Link></span>
-                        </div>
-                        <div>
-                            <span className="NavBar-element">{authedUserName}</span>
-                            <span className="NavBar-element NavBar-elementEnd"><button type="submit">Logout</button></span>
-                        </div>
-                    </div>) : null
-                }
+                <div className="NavBar-navigation">
+                    <div>
+                        <span className="NavBar-element"><Link to="/">Home</Link></span>
+                        <span className="NavBar-element"><Link to="/leaderboard">Leaderboard</Link></span>
+                        <span className="NavBar-element"><Link to="/add">Add Question</Link></span>
+                    </div>
+                    <div>
+                        {authedUserName && <span className="NavBar-element">{authedUserName}</span>}
+                        <span className="NavBar-element NavBar-elementEnd"><button type="submit">{isValidLogin && authedUserName ? "Logout" : "Login"}</button></span>
+                    </div>
+                </div>
             </div>
         </form>
     );

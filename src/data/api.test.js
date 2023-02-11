889c19738183a7ec5f83abbd79eaaed3ff568ab5
Divya Ambadipudi <divya.ambadipudi@clever.com>
2023-02-11 14:26:48 -0800

    Added tests for _DATA functions

--- src/data/api.test.js
+++ src/data/api.test.js
@@ -1,14 +1,13 @@
+import {_saveQuestion, _saveQuestionAnswer } from "./_DATA";
 
-import { saveQuestion, saveQuestionAnswer } from "./api";
-
-describe('saveQuestion', () => {
+describe('_saveQuestion', () => {
     it('will return the saved question correctly if valid parameters are passed in', async() => {
         var question = {
             optionOneText: "option one",
             optionTwoText: "option two",
             author: "sarahedo",
         };
-        var result = await saveQuestion(question);
+        var result = await _saveQuestion(question);
         expect(result.optionOne.text).toEqual(question.optionOneText);
         expect(result.optionTwo.text).toEqual(question.optionTwoText);
         expect(result.author).toEqual(question.author);
@@ -16,7 +15,7 @@ describe('saveQuestion', () => {
 
     it("will return an error if an empty object is passed", async() => {
         var question = {};
-        await expect(saveQuestion(question)).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+        await expect(_saveQuestion(question)).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
     });
 
     it("will return an error if the optionOne is missing in the question", async() => {
@@ -24,7 +23,7 @@ describe('saveQuestion', () => {
             optionTwoText: "option two",
             author: "author",
         };
-        await expect(saveQuestion(question)).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+        await expect(_saveQuestion(question)).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
     });
 
     it("will return an error if the optionTwo is missing in the question", async() => {
@@ -32,7 +31,7 @@ describe('saveQuestion', () => {
             optionOneText: "option one",
             author: "author",
         };
-        await expect(saveQuestion(question)).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+        await expect(_saveQuestion(question)).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
     });
 
     it("will return an error if the author is missing in the question", async() => {
@@ -40,24 +39,24 @@ describe('saveQuestion', () => {
             optionOneText: "option one",
             optionTwoText: "option two",
         };
-        await expect(saveQuestion(question)).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+        await expect(_saveQuestion(question)).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
     });
 });
 
-describe('saveQuestionAnswer', () => {
+describe('_saveQuestionAnswer', () => {
     it('will return true after answer to a question is saved', async() => {
         var questionAnswer = {
             authedUser: "sarahedo",
             qid: "8xf0y6ziyjabvozdd253nd",
             answer: "optionOne",
         };
-        var result = await saveQuestionAnswer(questionAnswer);
+        var result = await _saveQuestionAnswer(questionAnswer);
         expect(result).toEqual(true);
     });
 
     it("will return an error if an empty object is passed", async() => {
         var questionAnswer = {};
-        await expect(saveQuestionAnswer(questionAnswer)).rejects.toEqual('Please provide authedUser, qid, and answer');
+        await expect(_saveQuestionAnswer(questionAnswer)).rejects.toEqual('Please provide authedUser, qid, and answer');
     });
 
     it("will return an error if authedUser is missing", async() => {
@@ -65,7 +64,7 @@ describe('saveQuestionAnswer', () => {
             qid: "question id",
             answer: "optionOne",
         };
-        await expect(saveQuestionAnswer(questionAnswer)).rejects.toEqual('Please provide authedUser, qid, and answer');
+        await expect(_saveQuestionAnswer(questionAnswer)).rejects.toEqual('Please provide authedUser, qid, and answer');
     });
 
     it("will return an error if qid is missing", async() => {
@@ -73,7 +72,7 @@ describe('saveQuestionAnswer', () => {
             authedUser: "logged in user",
             answer: "optionOne",
         };
-        await expect(saveQuestionAnswer(questionAnswer)).rejects.toEqual('Please provide authedUser, qid, and answer');
+        await expect(_saveQuestionAnswer(questionAnswer)).rejects.toEqual('Please provide authedUser, qid, and answer');
     });
 
     it("will return an error if answer is missing", async() => {
@@ -81,6 +80,6 @@ describe('saveQuestionAnswer', () => {
             authedUser: "logged in user",
             qid: "question id",
         };
-        await expect(saveQuestionAnswer(questionAnswer)).rejects.toEqual('Please provide authedUser, qid, and answer');
+        await expect(_saveQuestionAnswer(questionAnswer)).rejects.toEqual('Please provide authedUser, qid, and answer');
     });
 });

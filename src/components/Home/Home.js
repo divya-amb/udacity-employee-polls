a104e209710a2075b43caccf4f1f498045cd2385
Divya Ambadipudi <divya.ambadipudi@clever.com>
2023-02-11 13:57:48 -0800

    Added tabs to the Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,41 @@
 import { connect } from "react-redux";
+import { useState } from "react";
 import Question from "../Question/Question";
 import "./Home.css";
 
+const ANSWER_TYPE = {
+    ANSWERED: "answered",
+    UNANSWERED: "unanswered",
+};
+
 const Home = ({ answeredQuestionIDs, unansweredQuestionIDs }) => {
+    const [showAnswerType, setShowAnswerType] = useState(ANSWER_TYPE.UNANSWERED);
+    
+    let idsToConsider = unansweredQuestionIDs;
+    let title = "Unanswered Polls";
+    let unansweredClassName = "active";
+    let answeredClassName = "";
+
+    if (showAnswerType === ANSWER_TYPE.ANSWERED) {
+        idsToConsider = answeredQuestionIDs;
+        title = "Answered Polls";
+        unansweredClassName = "";
+        answeredClassName = "active";
+    }
+    
+
     return (
         <>
-            <div className="Home-container">
-                <h3>Unanswered Polls</h3>
-                <div className="Home-content">
-                    {unansweredQuestionIDs.map((quesID) => (
-                       <Question key={quesID} questionID={quesID} />
-                    ))}
-                </div>
+            <div class="Home-tab">
+                <button onClick={() => setShowAnswerType(ANSWER_TYPE.UNANSWERED)} className={unansweredClassName}>Unanswered Polls</button>
+                <button onClick={() => setShowAnswerType(ANSWER_TYPE.ANSWERED)} className={answeredClassName}>Answered Polls</button>
             </div>
+
             <div className="Home-container">
-                <h3>Answered Polls</h3>
+                <h3>{title}</h3>
                 <div className="Home-content">
-                    {answeredQuestionIDs.map((quesID) => (
-                        <Question key={quesID} questionID={quesID} />
+                    {idsToConsider.map((quesID) => (
+                       <Question key={quesID} questionID={quesID} />
                     ))}
                 </div>
             </div>
